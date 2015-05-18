890ab45a6215dec891bc1bcd3be8ea1523e151cd
kornel84 <cornel_lus@yahoo.com>
2015-05-18 11:27:32 +0200

    yaaa :8ball:

--- SecondHandCars/src/secondhandcars/SecondHandCars.java
+++ SecondHandCars/src/secondhandcars/SecondHandCars.java
@@ -16,7 +16,7 @@ public class SecondHandCars {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Testings");
+        System.out.println("Balls");
     }
     
 }
