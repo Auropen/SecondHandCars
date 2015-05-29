6c1bb82d2607915a5a4465895505f2b4382f2925
Auropen <sonixbock@hotmail.com>
2015-05-29 09:50:30 +0200

    Sold Car Function is BACK #35

--- SecondHandCars/src/secondhandcars/ui/gui/SoldCars.java
+++ SecondHandCars/src/secondhandcars/ui/gui/SoldCars.java
@@ -5,16 +5,22 @@
  */
 package secondhandcars.ui.gui;
 
+import secondhandcars.application.IController;
+
 /**
  *
  * @author Cornel
  */
 public class SoldCars extends javax.swing.JFrame {
+    MainWindow mainWindow;
+    IController ctr;
 
     /**
      * Creates new form SoldCars
      */
-    public SoldCars() {
+    public SoldCars(IController ctr, MainWindow carWindow) {
+        this.ctr = ctr;
+        this.mainWindow = mainWindow;
         initComponents();
         removeGetCarsPanel();
     }
@@ -50,7 +56,7 @@ public class SoldCars extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btnGetAllCars.setText("Get all cars");
+        btnGetAllCars.setText("Get sold cars");
         btnGetAllCars.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnGetAllCarsActionPerformed(evt);
@@ -206,44 +212,10 @@ public class SoldCars extends javax.swing.JFrame {
     }//GEN-LAST:event_btnSearchGetCarsActionPerformed
 
     private void btnGetAllCarsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGetAllCarsActionPerformed
+        mainWindow.displayCars(ctr.getSoldCars());
         this.dispose();
     }//GEN-LAST:event_btnGetAllCarsActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SoldCars.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SoldCars.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SoldCars.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SoldCars.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SoldCars().setVisible(true);
-            }
-        });
-    }
-    
     public void removeGetCarsPanel(){
         panelGetCars.setVisible(false);
     }
